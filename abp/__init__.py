70fa7d7f77b545973f3a9b48c129efb3d8763b7f
Anurag Koul <koulanurag@gmail.com>
2018-02-15 15:31:11 -0800

    Merged updates from master

--- abp/__init__.py
+++ abp/__init__.py
@@ -1,11 +1,6 @@
-# from abp.openai import envs
-#
-# from abp.adaptives import DQNAdaptive, QAdaptive, HRAAdaptive, DQAdaptive
-# from abp.predictors import QPredictor
-#
-# __all__ = [DQNAdaptive, QAdaptive, HRAAdaptive, DQAdaptive, QPredictor]
-
 from abp.openai import envs
-from abp.adaptives import DQNAdaptive, HRAAdaptive
 
-__all__ = [DQNAdaptive, HRAAdaptive]
+from abp.adaptives import DQNAdaptive, QAdaptive, HRAAdaptive, DQAdaptive
+from abp.predictors import QPredictor
+
+__all__ = [DQNAdaptive, QAdaptive, HRAAdaptive, DQAdaptive, QPredictor]
