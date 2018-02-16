513bcfa59c553a53d9f74835fa6634c58a0427fe
Anurag Koul <koulanurag@gmail.com>
2018-02-15 16:03:34 -0800

    Merged updates from master

--- abp/examples/open_ai/cart_pole/hra.py
+++ abp/examples/open_ai/cart_pole/hra.py
@@ -11,14 +11,14 @@ def run_task(evaluation_config, network_config, reinforce_config):
     threshold_angle = 0.087266463
     threshold_x = 1.5
 
-    LEFT, RIGHT = [0,1]
+    LEFT, RIGHT = [0, 1]
 
     agent = HRAAdaptive(name="cartpole",
                         choices=[LEFT, RIGHT],
                         network_config=network_config,
                         reinforce_config=reinforce_config)
 
-    #Episodes
+    # Episodes
     for epoch in range(evaluation_config.training_episodes):
         state = env.reset()
         for steps in range(max_episode_steps):
@@ -26,11 +26,10 @@ def run_task(evaluation_config, network_config, reinforce_config):
             state, reward, done, info = env.step(action)
             cart_position, cart_velocity, pole_angle, pole_velocity = state
 
-            d_reward = [0]*3
+            d_reward = [0] * 3
 
-            
             # Reward for pole angle increase or decrease
-            if  -threshold_angle < pole_angle < threshold_angle:
+            if -threshold_angle < pole_angle < threshold_angle:
                 d_reward[0] = 1
             else:
                 d_reward[0] = -1
@@ -48,7 +47,6 @@ def run_task(evaluation_config, network_config, reinforce_config):
             if done:
                 agent.end_episode(state)
                 break
-
 
     agent.disable_learning()
 
