49f5905276207560df713285318cb533dd8cd766
Ema93sh <m.magesh.66@gmail.com>
2018-05-25 16:41:03 -0700

    Code Cleanup

--- abp/utils/__init__.py
+++ abp/utils/__init__.py
@@ -1,5 +1,4 @@
 from .summaries import clear_summary_path, saliency_to_excel
 from .logger import setup_custom_logger
-from .explanation import Explanation
 
 __all__ = [clear_summary_path, setup_custom_logger, saliency_to_excel]
