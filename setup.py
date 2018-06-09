05466b12f71e57b6feeca4d17e05ab8eec3904ff
Ema93sh <m.magesh.66@gmail.com>
2018-06-09 01:56:26 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ["torch", "gym", "numpy", "visdom", "tensorboardX"]
+REQUIRED_PACKAGES = ["torch==0.4", "gym", "numpy", "visdom", "tensorboardX"]
 
 
 setup(name='abp',
       version='0.1',
       description='Reinforcement Learning Library in python',
-      author='Magesh Kumar',
-      author_email='muralim@oregonstate.edu',
+      author='Magesh Kumar Murali',
+      author_email='m.magesh.66@gmail.com',
       include_package_data=True,
       package_data={'': ['tasks']},
       packages=find_packages(),
